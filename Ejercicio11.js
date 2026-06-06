4597158aa1e01c7fa8254413f3c9ae338668e4b4
Deiverlyn Leal <Deiverlynleal2020@gmail.com>
2026-06-06 09:48:36 -0500

    ultimo commit

--- Ejercicio11.js
+++ Ejercicio11.js
@@ -8,4 +8,5 @@ let numero=6;
 while (numero<=n){
     console.log(numero);
     numero=numero+3;// va aumentando de 3 en tres los multiplos
+    numero=numero+2;    // va aumentando de 2 en dos para que no imprima los multiplos de 2
 }
